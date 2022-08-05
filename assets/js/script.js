e8af6745fab6a72c3f57a7fdd8017ef0ea02d653
JGarcSoar <garciasoares.joe@gmail.com>
2022-08-05 12:29:01 -0700

    overall changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,62 @@
-// TODO: Declare any global variables we need
+// global variables
+let heads = 0;
+let headsPercent = 0;
+let tails = 0;
+let tailsPercent = 0;
+let total = heads + tails;
+let startMessage = "Let's Get Flipping!";
+let tailsMessage = "You Flipped Tails!";
+let headsMessage = "You Flipped Heads!";
 
-
-document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
-
-    // TODO: Add event listener and handler for flip and clear buttons
-
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
-
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
-
-
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
-
-})
+// DOM content failsafe
+document.addEventListener("DOMContentLoaded", function () {
+  // you flipped tails
+  document.querySelector("#flip").addEventListener("click", function () {
+    let randomChance = Math.floor(Math.random() * 2) + 1;
+    if (randomChance > 1) {
+      document.querySelector("#penny-image").src =
+        "./assets/images/penny-tails.jpg";
+      document.querySelector("#message").textContent = `${tailsMessage}`;
+      tails++;
+      total++;
+      tailsPercent = Math.round((tails / total) * 100);
+      document.querySelector("#tails").textContent = tails;
+      document.querySelector("#tails-percent").textContent = tailsPercent + "%";
+      if (heads > 0) {
+        headsPercent = Math.round((heads / total) * 100);
+        document.querySelector("#heads-percent").textContent =
+          headsPercent + "%";
+      }
+    } else {
+      // you flipped heads
+      document.querySelector("#penny-image").src =
+        "./assets/images/penny-heads.jpg";
+      document.querySelector("#message").textContent = `${headsMessage}`;
+      heads++;
+      total++;
+      headsPercent = Math.round((heads / total) * 100);
+      document.querySelector("#heads").textContent = heads;
+      document.querySelector("#heads-percent").textContent = headsPercent + "%";
+      if (tails > 0) {
+        tailsPercent = Math.round((tails / total) * 100);
+        document.querySelector("#tails-percent").textContent =
+          tailsPercent + "%";
+      }
+    }
+  });
+  // clear scoreboard and reset all values
+  document.querySelector("#clear").addEventListener("click", function () {
+    heads = 0;
+    headsPercent = 0;
+    tails = 0;
+    tailsPercent = 0;
+    total = heads + tails;
+    document.querySelector("#tails").textContent = 0;
+    document.querySelector("#tails-percent").textContent = 0 + "%";
+    document.querySelector("#heads").textContent = 0;
+    document.querySelector("#heads-percent").textContent = 0 + "%";
+    document.querySelector("#message").textContent = `${startMessage}`;
+    document.querySelector("#penny-image").src =
+      "./assets/images/penny-heads.jpg";
+  });
+});
